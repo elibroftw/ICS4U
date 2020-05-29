1d098d87b29aec28b198f2ff2df143ecc863b633
Elijah Lopez <elijahllopezz@gmail.com>
2020-05-28 22:33:12 -0400

    Added build.py

---
+++ build.py
@@ -0,0 +1,47 @@
+import subprocess
+import os
+import sys
+from contextlib import suppress
+
+
+OPTIONS = ['COMPILE', 'RUN', 'BUILD']
+OPTION = 'COMPILE'
+with suppress(IndexError):
+    if sys.argv[1] == 'build': OPTION = 'BUILD'
+    elif sys.argv[1] == 'run': OPTION = 'RUN'
+    else: OPTION = 'COMPILE'
+    
+
+libs = {
+    'javaFX': {
+        'path': 'C:/Users/maste/AppData/Local/Programs/java/libs/javafx-sdk-14.0.1/lib',
+        'modules': {
+            'javafx.controls',
+            'javafx.fxml'
+        }
+    }
+}
+
+MODULE_PATH = libs['javaFX']['path']
+MODULES = ','.join(libs['javaFX']['modules'])
+
+SRC = 'src/animation'
+DEPENDENTS = ['src/animation/Animation.java', 'src/animation/SpriteData.java', 'src/animation/SpriteSheet.java']
+DEPENDENTS = " ".join(DEPENDENTS)
+MAIN_CLASS = 'src/animation/Main'
+BUILD_DIR = 'build/'
+COMPILE_DIR = f'{BUILD_DIR}compiled/'
+
+if OPTION == 'COMPILE':
+    # os.chdir(SRC)
+    compile_cmd = f'javac {DEPENDENTS} --module-path {MODULE_PATH} --add-modules {MODULES} {MAIN_CLASS + ".java"}'
+    print(compile_cmd)
+
+    subprocess.call(compile_cmd)
+elif OPTION == 'RUN':
+    print(MAIN_CLASS.replace('/', '.'))
+    run_cmd = f'java --module-path {MODULE_PATH} --add-modules {MODULES} {MAIN_CLASS}'
+    subprocess.call(run_cmd)
+elif OPTION == 'BUILD':
+    pass
+    # subprocess.call('jar cf ')
